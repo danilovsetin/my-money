3e09d55e5a6aa42453602b13bd698b17d38aeecc
danilovsetin <34036386+danilovsetin@users.noreply.github.com>
2021-02-15 16:25:43 -0300

    alteracoes

--- src/pages/Home/AdicionarMes.js
+++ src/pages/Home/AdicionarMes.js
@@ -1,20 +1,47 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import {Redirect} from 'react-router-dom'
 
 const AdicionarMes = () => {
+    const refAno = useRef()
+    const refMes = useRef()
+    const [redir, setRedir] = useState('')
+
+    const anoMin = 2019
+    const anoMax = 2022
+    const anos = []
+    const meses = []
+
+    for(var i = anoMin; i <= anoMax; i++){
+        anos.push(i)                
+    }
+
+    for(var i = 1; i <= 12; i++){
+        meses.push(i)                
+    }
+
+    const zeroPad = mes => {
+        if (mes < 10){
+            return '0'+mes
+        }
+        return mes
+    }
+
+    const verMes = () => {
+        setRedir(refAno.current.value + '-' + refMes.current.value)
+    }
+    if(redir !== ''){
+        return <Redirect to={'movimentacoes/'+redir} />
+    }
     return (
         <React.Fragment>
             <h2>Adicionar mês</h2>
-            <select>
-                <option value='2021'>2021</option>
-                <option value='2020'>2020</option>
-                <option value='2019'>2019</option>
+            <select ref={refAno}>
+                {anos.map(ano => <option key={ano} value={ano}>{ano}</option>)}                
             </select>
-            <select>
-                <option value='01'>01</option>
-                <option value='02'>02</option>
-                <option value='03'>03</option>
+            <select ref={refMes}>
+                {meses.map(zeroPad).map(mes => <option key={mes} value={mes}>{mes}</option>)}                                
             </select>
-            <button>Adicionar mês</button>
+            <button onClick={verMes}>Adicionar mês</button>
         </React.Fragment>
     )
 }
--- src/pages/Movimentacoes.js
+++ src/pages/Movimentacoes.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Rest from '../utils/rest'
 
 const baseURL = 'https://mymoney-e3011-default-rtdb.firebaseio.com/'
-const { useGet } = Rest(baseURL)
+const { useGet, usePost, useDelete } = Rest(baseURL)
 
 const Movimentacoes = ({match}) => {
     const data = useGet(`movimentacoes/${match.params.data}`)
+    const [postData, salvar] = usePost(`movimentacoes/${match.params.data}`)
+    const [postDelete, remover]  = useDelete()
+    const [valor, setValor] = useState('')
+    const [descricao, setDescricao] = useState('')
+
+    const onChangeValor = (evt) => {
+        setValor(evt.target.value)
+    }
+
+    const onChangeDescricao = (evt) => {
+        setDescricao(evt.target.value)
+    }
+
+    const salvarMovimentacao = async () => {
+        if(!isNaN(valor) && valor.search(/^[-]?\d+(\.)?\d+?$/) >= 0){
+            await salvar({
+                descricao,
+                valor: parseFloat(valor)
+            })
+            setDescricao('')
+            setValor('')
+            data.refetch()
+        }        
+    }
+
+    const excluirMovimentacao = async(id) => {
+        await remover(`movimentacoes/${match.params.data}/${id}`)        
+        data.refetch()
+    }
+
     if (data.loading) {
         return <div className='container'><span>Carregando...</span></div>
     }
@@ -27,16 +57,28 @@ const Movimentacoes = ({match}) => {
                 .map(mov => {
                 return(
                     <tr key={mov}>                    
-                    <td>{data.data[mov].descricao}</td>
-                    <td>{data.data[mov].valor}</td>                    
-                    </tr>
+                        <td>{data.data[mov].descricao}</td>
+                        <td>
+                            {data.data[mov].valor} {'  '}
+                            <button className='btn btn-danger' onClick={() => excluirMovimentacao(mov)}>-</button>
+                        </td>                           
+                    </tr>                    
                 )
-                })
+                })                
             }
             {
                 !data.data &&
                 <span>Não existem movimentações nesse período</span>
             }
+                <tr>
+                    <td>
+                        <input type='text' value={descricao} onChange={onChangeDescricao} />
+                    </td>
+                    <td>
+                        <input type='text' value={valor} onChange={onChangeValor} />
+                        <button className='btn btn-success' onClick={salvarMovimentacao}>+</button>
+                    </td>
+                </tr>
             </tbody>
         </table>        
       </div>
